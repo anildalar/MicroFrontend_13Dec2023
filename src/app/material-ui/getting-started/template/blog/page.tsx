f3e3a27996cb13c8c8468f6fc712dba3c773193a
anildalar <dollorinfotech@gmail.com>
2023-12-20 10:14:04 +0530

    new

--- src/app/material-ui/getting-started/template/blog/page.tsx
+++ src/app/material-ui/getting-started/template/blog/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 // 1 Import
-import { Box, Button, Container, CssBaseline, IconButton, ThemeProvider, Toolbar, Typography, createTheme } from '@mui/material';
+import { Box, Button, Container, CssBaseline, Divider, Grid, IconButton, Link, Paper, Stack, ThemeProvider, Toolbar, Typography, createTheme } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import React from 'react'
 import NLink from 'next/link';
@@ -24,27 +24,12 @@ function Blog() {
     <ThemeProvider theme={themeObject}>
       <CssBaseline/>
       <Container className="a_tbdr" maxWidth="lg">
-        <Box component="header">
-
-          <Toolbar className="a_tbdr">
-            <Button variant="outlined" size="small">Subscribe</Button>
-            <Typography variant="h5" noWrap  align="center" sx={{ flex: 1 }}>Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1Blog1</Typography>
-            <IconButton>
-              <SearchIcon />
-            </IconButton>
-            <Button variant="outlined" size="small">SignUp</Button>
-          </Toolbar>
-          <Toolbar component="nav">
-            {
-             MENUS.map((cv,index,arr)=>{
-              return <MULink key={index} href="#" sx={{p:2}}>{cv.name}</MULink>
-             }) 
-            }
-              
-          </Toolbar>
-        </Box>
-        <Box component="main">main</Box>
-        <Box component="footer">Footer</Box>
+        <Stack>
+              <Link href="#">Active</Link>
+              <Link href="#">Link</Link>
+              <Link href="#">Link</Link>
+              <Link href="#">Disable</Link>
+        </Stack>
       </Container>
       
     </ThemeProvider>
